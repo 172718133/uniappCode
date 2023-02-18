65ff5150ad6296dbf4eb3763d1e5cbec0e88c07d
YiLong <2488174858@qq.com>
2023-02-18 16:50:02 +0800

    完成轮播图、导航

--- UGO/api/index.js
+++ UGO/api/index.js
@@ -1,13 +1,28 @@
 import {request} from '@/util/request.js'
 
+/**
+ * 获取轮播图
+ */
 export const getSwiperAPI = () => {
 	return request({
 		url: '/home/swiperdata'
 	})
 }
 
+/**
+ * 获取导航
+ */
 export const getCatitemAPI = () => {
 	return request({
 		url: '/home/catitems'
+	})
+}
+
+/**
+ * 获取楼层
+ */
+export const getFloorAPI = () => {
+	return request({
+		url: '/home/floordata'
 	})
 }
--- UGO/pages/index/index.vue
+++ UGO/pages/index/index.vue
@@ -1,24 +1,32 @@
 <template>
 	<view class="index">
+		<!-- 轮播图 -->
 		<div class="swiper">
-			<swiper indicator-dots circular autoplay interval="3000" duration="500">
-				<swiper-item v-for="item in swiperList">
+			<swiper indicator-dots circular autoplay interval="3000" duration="500" indicator-active-color="#fff">
+				<swiper-item v-for="item in swiperList" :key="item.goods_id">
 					<view class="swiper-item">
 						<image :src="item.image_src" mode="widthFix"></image>
 					</view>
 				</swiper-item>
 			</swiper>
 		</div>
+		<!-- 导航 -->
+		<div class="cat">
+			<div class="catitem" v-for="item in catitemList" :key="item.name">
+				<image :src="item.image_src" mode="widthFix"></image>
+			</div>
+		</div>
 	</view>
 </template>
 
 <script>
-	import { getSwiperAPI, getCatitemAPI } from '@/api/index.js'
+	import { getSwiperAPI, getCatitemAPI, getFloorAPI } from '@/api/index.js'
 	export default {
 		data() {
 			return {
 				swiperList: {},
-				catitemList: {}
+				catitemList: {},
+				floorList: {}
 			}
 		},
 		methods: {
@@ -29,21 +37,41 @@
 			async getCatitem () {
 				const {data: res} = await getCatitemAPI()
 				this.catitemList = res.message
-				console.log(this.catitemList);
+			},
+			async getFloor () {
+				const {data: res} = await getFloorAPI()
+				this.floorList = res.message
+				console.log(this.floorList);
 			}
 		},
 		onLoad() {
-			this.getSwiper(),
+			this.getSwiper()
 			this.getCatitem()
+			this.getFloor()
 		}
 	}
 </script>
 
 <style lang="less" scoped>
-	.swiper-item {
+	.swiper swiper {
 		width: 100%;
-		image {
-			width: 100%;
+		height: 340rpx;
+		.swiper-item {
+			// width: 100%;
+			image {
+				width: 100%;
+			}
+		}
+	}
+	.cat {
+		display: flex;
+		margin: 10px 0;
+		.catitem {
+			text-align: center;
+			flex: 1;
+			image {
+				width: 160rpx;
+			}
 		}
 	}
 </style>
